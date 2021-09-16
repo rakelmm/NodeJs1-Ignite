6f77b08afe0204c293e0d08e8bf938586c3ab42d
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2021-09-16 13:51:26 -0300

    src/index.js modifiend

--- src/index.js
+++ src/index.js
@@ -2,22 +2,39 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 /**
  * GET - Buscar uma informacao dentro do servidor
  * POST - Iserir uma informaçao no servidor
  * PUT - Alterar uma informaçao no servidor
  * DELETE - Deletar uam informação no servidor
+
  */
 
-app.get("/coursers", (request, response) => {
+
+/**
+ * Tipos de parâmentros
+ * Routes Params => serve para identificar um recurso para poder editar/deletar/buscar
+ *  Body Params => Os objetos inserção/alteração (JSON no insomnia)
+ * 
+ */
+
+app.get("/courses", (request, response) => {
+  const query = request.query;
+  console.log(query);
   return response.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 
-app.post("/coursers", (request, response) => {
+app.post("/courses", (request, response) => {
+  const body = request.body;
+  console.log(body);
   return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
-app.put("/coursers/:id", (request, response) => {
+app.put("/courses/:id", (request, response) => {
+  const { id } = request.params;
+  console.log(id);
   return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
@@ -25,8 +42,8 @@ app.patch("/courses/:id", (request, response) => {
   return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
 });
 
-app.delete("courses/:id", (request, response) => {
-  return response.json(["Curso 6", "Curso 2", "Curso 4"])
-})
+app.delete("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 2", "Curso 4"]);
+});
 
 app.listen(3333);
